67687778a35421ea3a4b96eace7cc6afaa0bf500
AlexPasqua <alexpasquali98@gmail.com>
2021-03-17 10:09:50 +0100

    Load and sort data + updated readme

--- midterm1/autoregressive_model.m
+++ midterm1/autoregressive_model.m
@@ -1,4 +1,5 @@
-% import the data
+% import the data and sort the data by date
 appliances = readtable("energydata_complete.csv");
+appliances = sortrows(appliances, "date");
 appliances = appliances(:, "Appliances");
 appliances = appliances{:,:};
