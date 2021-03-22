47b0672f7b2304428629733b54f4b455ce3997f7
AlexPasqua <alexpasquali98@gmail.com>
2021-03-22 18:26:13 +0100

    Created cycle for order selection

--- midterm1/my_utilities.py
+++ midterm1/my_utilities.py
@@ -5,20 +5,17 @@ import datetime
 
 
 def read_data():
-    # read the data
+    # read the data and sort it by date
     whole_data = pd.read_csv("energydata_complete.csv")
     whole_data = whole_data[["date", "Appliances"]]
     whole_data["date"] = pd.to_datetime(whole_data["date"])
     whole_data = whole_data.sort_values(by="date")
 
-    # select training data
+    # select training and test data. Then drop the date column
     first_day = whole_data["date"][0]
     last_day = first_day + datetime.timedelta(weeks=13)
     tr_data = whole_data[(whole_data["date"] >= first_day) & (whole_data["date"] <= last_day)]["Appliances"]
     test_data = whole_data[whole_data["date"] > last_day]["Appliances"]
     whole_data = whole_data["Appliances"]
 
-    # autocorrelation_plot(tr_data)
-    # plt.show()
-
-    return whole_data.to_list(), tr_data.to_list(), test_data.to_list()
+    return whole_data.to_numpy(), tr_data.to_numpy(), test_data.to_numpy()
